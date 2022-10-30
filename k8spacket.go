c24d174f34aef9e5da2921c482167705a0c271d4
k8spacket <k8spacket@gmail.com>
2022-10-30 13:29:59 +0100

    reorganize prometheus metrics

--- k8spacket.go
+++ k8spacket.go
@@ -5,6 +5,8 @@ import (
 	"github.com/k8spacket/k8spacket/broker"
 	"github.com/k8spacket/k8spacket/plugins"
 	"github.com/k8spacket/k8spacket/tcp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
@@ -16,8 +18,15 @@ func main() {
 	plugins.InitPlugins(pluginManager)
 	go broker.DistributeMessages(pluginManager)
 	tcp.StartListeners()
+	handleEndpoints()
+}
+
+func handleEndpoints() {
 	listenerPort := os.Getenv("K8S_PACKET_TCP_LISTENER_PORT")
 	log.Printf("Serving requests on port %s", listenerPort)
+
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
 	http.Handle("/metrics", promhttp.Handler())
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", listenerPort), nil))
 }
