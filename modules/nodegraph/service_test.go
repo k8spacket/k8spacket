c5bb241ce548820cadf9ada84a75be1ade5f3e15
Darek <bareckidarek@gmail.com>
2024-08-29 16:06:45 +0200

    tests

--- modules/nodegraph/service_test.go
+++ modules/nodegraph/service_test.go
@@ -3,11 +3,11 @@ package nodegraph
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
-	"net/url"
 	"regexp"
 	"testing"
 	"time"
@@ -56,11 +56,44 @@ type mockHttpClient struct {
 }
 
 func (httpClient *mockHttpClient) Do(req *http.Request) (*http.Response, error) {
-	json, _ := json.Marshal(dbState)
-	t := http.Response{
-		Body: io.NopCloser(bytes.NewBuffer(json)),
-    }
-	return &t, nil
+
+	if req.URL.Query().Get("scenario") == "ok" {
+		result, _ := json.Marshal(dbState)
+		return &http.Response{
+			Body: io.NopCloser(bytes.NewBuffer(result)),
+		}, nil
+	}
+	if req.URL.Query().Get("scenario") == "error" {
+		response := []model.ConnectionItem{}
+		err := errors.New("error")
+		result, _ := json.Marshal(response)
+		return &http.Response{
+			Body: io.NopCloser(bytes.NewBuffer(result)),
+		}, err
+	}
+	if req.URL.Query().Get("scenario") == "read" {
+		reader := BrokenReader{}
+		return &http.Response{
+			Body: &reader,
+		}, nil
+	}
+	if req.URL.Query().Get("scenario") == "parse" {
+		result := []byte("parse error")
+		return &http.Response{
+			Body: io.NopCloser(bytes.NewBuffer(result)),
+		}, nil
+	}
+	return &http.Response{}, nil
+}
+
+type BrokenReader struct{}
+
+func (br *BrokenReader) Read(p []byte) (n int, err error) {
+    return 0, fmt.Errorf("failed reading")
+}
+
+func (br *BrokenReader) Close() error {
+    return fmt.Errorf("failed closing")
 }
 
 
@@ -91,25 +124,48 @@ func TestGetConnections(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	item := model.ConnectionItem{Src: "src", Dst: "dst", ConnCount: 10, ConnPersistent: 5, BytesReceived: 1000, BytesSent: 500, Duration: 0.5, MaxDuration: 0.5}
-	want := model.ConnectionItem{Src:"src", SrcName:"srcName", SrcNamespace:"srcNs", Dst:"dst", DstName:"dstName", DstNamespace:"dstNs", ConnCount:11, ConnPersistent:6, BytesSent:600, BytesReceived:1200, Duration:1.5, MaxDuration:1}
+	var tests = []struct {
+		item model.ConnectionItem
+		want     model.ConnectionItem
+	}{
+		{model.ConnectionItem{Src: "src", Dst: "dst", ConnCount: 10, ConnPersistent: 5, BytesReceived: 1000, BytesSent: 500, Duration: 0.5, MaxDuration: 0.5}, 
+			model.ConnectionItem{Src:"src", SrcName:"srcName", SrcNamespace:"srcNs", Dst:"dst", DstName:"dstName", DstNamespace:"dstNs", ConnCount:11, ConnPersistent:6, BytesSent:600, BytesReceived:1200, Duration:1.5, MaxDuration:1}},
+		{model.ConnectionItem{},
+			model.ConnectionItem{Src:"src", SrcName:"srcName", SrcNamespace:"srcNs", Dst:"dst", DstName:"dstName", DstNamespace:"dstNs", ConnCount:1, ConnPersistent:1, BytesSent:100, BytesReceived:200, Duration:1, MaxDuration:1}},
+	}
 
+	for _, test := range tests {
+		t.Run(test.item.Src, func(t *testing.T) {
 
-	mockRepository := &mockRepository{result: item}
-	factory := &stats.Factory{}
-	service := &Service{mockRepository, factory, &httpclient.HttpClient{}, &k8sclient.K8SClient{}}
+		mockRepository := &mockRepository{result: test.item}
+		factory := &stats.Factory{}
+		service := &Service{mockRepository, factory, &httpclient.HttpClient{}, &k8sclient.K8SClient{}}
 
-	service.update(item.Src, "srcName", "srcNs", item.Dst, "dstName", "dstNs", true, 100, 200, 1)
+		service.update("src", "srcName", "srcNs", "dst", "dstName", "dstNs", true, 100, 200, 1)
 
-	result := mockRepository.Read("")
-	
-	want.LastSeen = result.LastSeen
-	assert.EqualValues(t, want, result)
+		result := mockRepository.Read("")
+		
+		test.want.LastSeen = result.LastSeen
+		assert.EqualValues(t, test.want, result)
+		})
+	}
 }
 
 func TestBuildO11yResponse(t *testing.T) {
 
-	want := model.NodeGraph{
+	var str bytes.Buffer
+
+	logger := slog.New(slog.NewTextHandler(&str, nil))
+
+	slog.SetDefault(logger)
+
+	var tests = []struct {
+		scenario string
+		want     *model.NodeGraph
+		err      string
+	}{
+
+	{"ok", &model.NodeGraph{
 		Nodes:[]model.Node{
 			model.Node{Id:"", Title:"test", SubTitle:"", MainStat:"all: N/A", SecondaryStat:"persistent: N/A", Arc1:0, Arc2:0, Arc3:0}, 
 			model.Node{Id:"test", Title:"", SubTitle:"test", MainStat:"all: N/A", SecondaryStat:"persistent: N/A", Arc1:0, Arc2:0, Arc3:0}, 
@@ -120,19 +176,33 @@ func TestBuildO11yResponse(t *testing.T) {
 		Edges:[]model.Edge{
 			model.Edge{Id:"-test", Source:"", Target:"test", MainStat:"all: 0", SecondaryStat:"persistent: 0"}, 
 			model.Edge{Id:"test-", Source:"test", Target:"", MainStat:"all: 0", SecondaryStat:"persistent: 0"}, 
-			model.Edge{Id:"-", Source:"", Target:"", MainStat:"all: 0", SecondaryStat:"persistent: 0"}}}
+			model.Edge{Id:"-", Source:"", Target:"", MainStat:"all: 0", SecondaryStat:"persistent: 0"}}}, ""},
+			{"error", &model.NodeGraph{}, "[api] Cannot get stats"},
+			{"read", &model.NodeGraph{}, "[api] Cannot read stats response"},
+			{"parse", &model.NodeGraph{}, "[api] Cannot parse stats response"},
+	}
 
 	mockRepository := &mockRepository{}
 	factory := &stats.Factory{}
 	service := &Service{mockRepository, factory, &mockHttpClient{}, &mockK8SClient{}}
 
+	
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) {
 
-	r := http.Request{}
-	r.URL = &url.URL{}
-	r.URL.Query().Set("stats-type", "connection")
+			r, _ := http.NewRequest(http.MethodGet, "", nil)
 
-	result, _ := service.buildO11yResponse(&r)
+			q := r.URL.Query()
+			q.Set("stats-type", "connection")
+			q.Set("scenario", test.scenario)
+			r.URL.RawQuery = q.Encode()
 
-	assert.ElementsMatch(t, want.Nodes, result.Nodes)
-	assert.ElementsMatch(t, want.Edges, result.Edges)
+			result, _ := service.buildO11yResponse(r)
+
+			assert.ElementsMatch(t, test.want.Nodes, result.Nodes)
+			assert.ElementsMatch(t, test.want.Edges, result.Edges)
+
+			assert.Contains(t, str.String(), test.err)
+		})
+	}
 }
