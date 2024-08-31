3f7479a16060296cb2daece4d2617be68f453229
Darek <bareckidarek@gmail.com>
2024-08-31 10:16:33 +0200

    tests

--- modules/nodegraph/injector_test.go
+++ modules/nodegraph/injector_test.go
@@ -1,12 +1,15 @@
 package nodegraph
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestInit(t *testing.T) {
+
+	os.Setenv("K8S_PACKET_TCP_METRICS_ENABLED", "true")
 
 	listener := Init()
 
