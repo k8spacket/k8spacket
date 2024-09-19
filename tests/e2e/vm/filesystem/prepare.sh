5a03f580cf29fef0f483ae8493e5701ddd0da7d5
k8spacket <k8spacket@gmail.com>
2024-09-19 19:09:37 +0200

    e2e preparations

--- tests/e2e/vm/filesystem/prepare.sh
+++ tests/e2e/vm/filesystem/prepare.sh
@@ -1,5 +1,8 @@
+# build filesystem image and store as tar archive
 DOCKER_BUILDKIT=1 docker build --output "type=tar,dest=filesystem.tar" .
 
+# convert tar to qcow2 image
 virt-make-fs --format=qcow2 --size=+100M filesystem.tar filesystem-large.qcow2
+# reduce size of image
 qemu-img convert filesystem-large.qcow2 -O qcow2 filesystem.qcow2
 rm filesystem-large.qcow2
