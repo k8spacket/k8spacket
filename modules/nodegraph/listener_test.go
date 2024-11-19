e7cf9a4b83a02354093bb675116f0e801d61fcbe
k8spacket <k8spacket@gmail.com>
2024-11-19 11:13:12 +0100

    issue 83 - ongoing connections

--- modules/nodegraph/listener_test.go
+++ modules/nodegraph/listener_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func (mockService *mockService) update(src string, srcName string, srcNamespace string, dst string, dstName string, dstNamespace string, persistent bool, bytesSent float64, bytesReceived float64, duration float64) {
+func (mockService *mockService) update(src string, srcName string, srcNamespace string, dst string, dstName string, dstNamespace string, persistent bool, bytesSent float64, bytesReceived float64, duration float64, closed bool) {
 	mockService.client = src
 	mockService.server = dst
 }
